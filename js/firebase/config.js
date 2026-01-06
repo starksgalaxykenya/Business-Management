120742143e874e832dc91f6f10053c147d4ffbc4
starksgalaxykenya <starksgalaxykenya@gmail.com>
2026-01-06 19:47:16 +0300

    Create config.js

---
+++ js/firebase/config.js
@@ -0,0 +1,13 @@
+// js/firebase/config.js
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBORBswvSauHGag4Zkp4f_If3Rx9zNiagU",
+  authDomain: "carskenyaapp.firebaseapp.com",
+  projectId: "carskenyaapp",
+  storageBucket: "carskenyaapp.firebasestorage.app",
+  messagingSenderId: "80734328",
+  appId: "1:80734328:web:2b4fbe06dabb73dad7258d",
+  measurementId: "G-8YYTLJVJK7"
+};
+
+export { firebaseConfig };
